e772eb55db22eccc9a1a7cc33f2eb648c29f6ec3
Ümit Yılmaz <umityilmaz1099@gmail.com>
2019-10-07 21:13:34 +0300

    ProjeOlusturuldu

---
+++ WebSocketWork/Server/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstbxConversation = new System.Windows.Forms.ListBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnGonder = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstbxConversation
+            // 
+            this.lstbxConversation.FormattingEnabled = true;
+            this.lstbxConversation.Location = new System.Drawing.Point(13, 13);
+            this.lstbxConversation.Name = "lstbxConversation";
+            this.lstbxConversation.Size = new System.Drawing.Size(637, 225);
+            this.lstbxConversation.TabIndex = 0;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(60, 245);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(590, 20);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // btnGonder
+            // 
+            this.btnGonder.Location = new System.Drawing.Point(575, 271);
+            this.btnGonder.Name = "btnGonder";
+            this.btnGonder.Size = new System.Drawing.Size(75, 23);
+            this.btnGonder.TabIndex = 2;
+            this.btnGonder.Text = "Gönder";
+            this.btnGonder.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 245);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mesaj :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 336);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGonder);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.lstbxConversation);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstbxConversation;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnGonder;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
