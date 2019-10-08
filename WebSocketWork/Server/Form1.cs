fa131ae95e256f90ce957b285d5551fc478dfecd
Ümit Yılmaz <umityilmaz1099@gmail.com>
2019-10-08 10:24:16 +0300

    push201910081024

--- WebSocketWork/Server/Form1.cs
+++ WebSocketWork/Server/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,24 @@ namespace Server
         public Form1()
         {
             InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        Socket socket;
+        NetworkStream stream;
+        TcpListener listener;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listener = new TcpListener(1453);
+            listener.Start();
+
+            socket = listener.AcceptSocket();
+            stream = new NetworkStream(socket);
+
+            Thread dinle = new Thread(soketDinle);
+            dinle.Start();
+
         }
     }
 }
